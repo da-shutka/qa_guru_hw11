2cac3138cc6d2902a2505be140d204cf61b0c55b
Daria Petrova <petrova.tpu@gmail.com>
2024-05-29 00:18:43 +0300

    HW-11: Added selenoid + updated allure verions

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.TableResultComponent;
 
@@ -31,6 +32,7 @@ public class RegistrationPage extends BasePage {
     CalendarComponent calendarComponent = new CalendarComponent();
     TableResultComponent modal = new TableResultComponent();
 
+    @Step("Открыть регистрационную форму")
     public RegistrationPage openPage() {
         open("automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -38,81 +40,97 @@ public class RegistrationPage extends BasePage {
         return this;
     }
 
+    @Step("Ввести имя")
     public RegistrationPage setFirstName(String firstName) {
         firstNameInput.setValue(firstName);
         return this;
     }
 
+    @Step("Ввести фамилию")
     public RegistrationPage setLastName(String lastName) {
         lastNameInput.setValue(lastName);
         return this;
     }
 
+    @Step("Ввести электронную почту")
     public RegistrationPage setUserEmail(String userEmail) {
         userEmailInput.setValue(userEmail);
         return this;
     }
 
+    @Step("Выбрать пол")
     public RegistrationPage setGender(String gender) {
         userGender.$(byText(gender)).click();
         return this;
     }
 
+    @Step("Ввести номер телефона")
     public RegistrationPage setUserNumber(String number) {
         userNumber.setValue(number);
         return this;
     }
 
+    @Step("Выбрать дату рождения")
     public RegistrationPage setDateOfBirth(List<String> birthDay) {
         calendarInput.click();
         calendarComponent.setDate(birthDay.get(0), birthDay.get(1), birthDay.get(2));
         return this;
     }
 
+    @Step("Задать предметы")
     public RegistrationPage setSubjectsInput(String subject) {
         subjectsInput.setValue(subject).pressEnter();
         return this;
     }
 
+    @Step("Выбрать хобби")
     public RegistrationPage setHobbies(String hobby) {
         hobbiesInput.$$("label").filterBy(text(hobby)).first().click();
         return this;
     }
 
+    @Step("Загрузить фото")
     public RegistrationPage uploadPicture(String fileName) {
         uploadPictureInput.uploadFromClasspath(fileName);
         return this;
     }
 
+    @Step("Ввести адрес проживания")
     public RegistrationPage setCurrentAddress(String address) {
         currentAddressInput.setValue(address);
         return this;
     }
 
+    @Step("Выбрать область")
     public RegistrationPage setState(String state) {
         stateInput.setValue(state).pressEnter();
         return this;
     }
 
+    @Step("Выбрать город")
     public RegistrationPage setCity(String city) {
         cityInput.setValue(city).pressEnter();
         return this;
     }
 
+    @Step("Зарегистрироваться")
     public void submit() {
         $("#submit").click();
     }
 
+    @Step("Проверить успешность регистрации")
     public RegistrationPage checkSuccessfulSubmit() {
         submittedForm.shouldHave(text("Thanks for submitting the form"));
         return this;
     }
 
+    @Step("Проверить поле {key}")
     public RegistrationPage checkResult(String key, String value) {
         modal.checkTableRow(key, value);
         return this;
     }
 
+    @Step("Проверить неуспешность регистрации")
     public void checkUnsuccessfulSubmit() {
         submittedForm.shouldBe(hidden);
     }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,6 +12,7 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
