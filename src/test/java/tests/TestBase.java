770c66bb1b2ef73a81b10da759d2b4ef5c2dba4d
Daria Petrova <petrova.tpu@gmail.com>
2024-05-29 01:13:07 +0300

    HW-11: Moved listner to beforeEach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,6 +7,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -26,7 +27,10 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    void beforeEach() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
